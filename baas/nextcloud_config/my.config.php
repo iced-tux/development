89bed0fb3a7fedaa1f4cb355d66d3ebc3c31f8d8
Christian Langer <ch.langer+github@posteo.de>
2018-12-02 13:18:27 +0100

    first nextcloud app config draft

---
+++ baas/nextcloud_config/my.config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array(
+
+'default_language' => 'de',
+'force_language' => 'de',
+'has_internet_connection' => false,
+'appstoreenabled' => false,
+ array (
+   0 =>
+   array (
+     'path' => OC::$SERVERROOT.'/apps',
+     'url' => '/apps',
+     'writable' => true,
+   ),
+   1 =>
+   array (
+     'path' => OC::$SERVERROOT.'/apps_custom',
+     'url' => '/apps_custom',
+     'writable' => true,
+   ),
+ ),
+
+);
