dc8fe37a6a3f541987d7df8cc785d97303e4df80
Christian Langer <ch.langer+github@posteo.de>
2018-12-07 06:58:00 +0100

    renamed from baas

---
+++ nc/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "nextcloud",
+  "dbuser"        => "nextcloud",
+  "dbpass"        => "FooBar",
+  "dbhost"        => "db",
+  "dbtableprefix" => "",
+  "adminlogin"    => "root",
+  "adminpass"     => "root",
+  "directory"     => "/var/www/html/data",
+);
